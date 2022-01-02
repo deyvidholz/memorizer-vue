21e7b817a27449719abd0d646d603fda1003b955
Deyvid Holz <deyvidholzt@gmail.com>
2022-01-02 05:07:10 -0300

    🎨 remove default scaffold

---
+++ src/components/app/AppBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <div class="d-flex align-center">Memorizer</div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn text>
+      <span class="mr-2">Button</span>
+    </v-btn>
+  </v-app-bar>
+</template>
