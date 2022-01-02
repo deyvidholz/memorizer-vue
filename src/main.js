d8163409b262662fc6d358749939a3f4b507c32c
Deyvid Holz <deyvidholzt@gmail.com>
2022-01-02 05:11:59 -0300

    ♻️ change default font

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
 
-Vue.config.productionTip = false
+// @note using global css
+import "./assets/styles/global.css";
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
